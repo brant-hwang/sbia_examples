36cf0cb8916c73aa5243644b12d4f41705b17b3f
Brant Hwang <dlstj3039@gmail.com>
2016-06-19 17:51:50 +0900

    update codes~

---
+++ ch04/readinglist-unsecured/src/test/java/readinglist/MockMvcWebTests.java
@@ -0,0 +1,87 @@
+package readinglist;
+
+import org.hamcrest.Matchers;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.hamcrest.Matchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = ReadingListApplication.class)
+@WebAppConfiguration
+public class MockMvcWebTests {
+
+    @Autowired
+    private WebApplicationContext webContext;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setupMockMvc() {
+        mockMvc = MockMvcBuilders
+            .webAppContextSetup(webContext)
+            .build();
+    }
+
+    // @Test
+    // public void homePage() throws Exception {
+    //     mockMvc.perform(MockMvcRequestBuilders.get("/"))
+    //         .andExpect(MockMvcResultMatchers.status().isOk())
+    //         .andExpect(MockMvcResultMatchers.view().name("readingList"))
+    //         .andExpect(MockMvcResultMatchers.model().attributeExists("books"))
+    //         .andExpect(MockMvcResultMatchers.model().attribute("books",
+    //             Matchers.is(Matchers.empty())));
+    // }
+
+	@Test
+	public void homePage() throws Exception {
+	    mockMvc.perform(get("/"))
+	        .andExpect(status().isOk())
+	        .andExpect(view().name("readingList"))
+	        .andExpect(model().attributeExists("books"))
+	        .andExpect(model().attribute("books", is(empty())));
+	}
+
+	@Test
+	public void postBook() throws Exception {
+		mockMvc.perform(post("/")
+		       .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+		       .param("title", "BOOK TITLE")
+		       .param("author", "BOOK AUTHOR")
+		       .param("isbn", "1234567890")
+		       .param("description", "DESCRIPTION"))
+		       .andExpect(status().is3xxRedirection())
+		       .andExpect(header().string("Location", "/"));
+
+		Book expectedBook = new Book();
+		expectedBook.setId(1L);
+		expectedBook.setReader("craig");
+		expectedBook.setTitle("BOOK TITLE");
+		expectedBook.setAuthor("BOOK AUTHOR");
+		expectedBook.setIsbn("1234567890");
+		expectedBook.setDescription("DESCRIPTION");
+
+		mockMvc.perform(get("/"))
+		       .andExpect(status().isOk())
+		       .andExpect(view().name("readingList"))
+		       .andExpect(model().attributeExists("books"))
+		       .andExpect(model().attribute("books", hasSize(1)))
+		       .andExpect(model().attribute("books",
+		               contains(samePropertyValuesAs(expectedBook))));
+	}
+	
+}
---
+++ ch07/readinglist-cli/Grabs.groovy
@@ -0,0 +1,5 @@
+@Grab('spring-boot-starter-actuator')
+@Grab("spring-boot-starter-remote-shell")
+@Grab("spring-boot-starter-thymeleaf")
+@Grab("h2")
+class Grabs {}
